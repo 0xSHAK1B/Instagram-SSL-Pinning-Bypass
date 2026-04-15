d76492bead1e1b1525bd7f3bdcf93c482f6bcb54
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-04-15 13:29:36 +0600

    Refactor HTTP request headers for GraphQL

--- traffic.sh
+++ traffic.sh
@@ -1,44 +1,28 @@
-POST /api/v1/bloks/async_action/com.bloks.www.bloks.caa.login.async.send_login_request/ HTTP/2
+POST /graphql_www HTTP/2
 
 host: b.i.instagram.com
 
 accept-language: en-US
 
-content-type: application/x-www-form-urlencoded; charset=UTF-8
+content-encoding: gzip
 
-ig-intended-user-id: 0
+content-length: 953
 
-priority: u=3
+content-type: application/x-www-form-urlencoded
 
-x-bloks-is-layout-rtl: false
+priority: u=3, i
 
-x-bloks-prism-button-version: CONTROL
+user-agent: Instagram 425.0.0.47.61 Android (30/11; 274dpi; 720x1640; Symphony; Z35; Z35; mt6765; en_US; 938255970)
 
-x-bloks-prism-colors-enabled: false
+x-bloks-version-id: 2a6e58f9d7d3bc66d326cab0c9a0c3fcba18cc5959ae6829d31a6817747cc017
 
-x-bloks-prism-extended-palette-gray: false
-
-x-bloks-prism-extended-palette-indigo: false
-
-x-bloks-prism-extended-palette-polish-enabled: false
-
-x-bloks-prism-extended-palette-red: false
-
-x-bloks-prism-extended-palette-rest-of-colors: false
-
-x-bloks-prism-font-enabled: true
-
-x-bloks-prism-indigo-link-version: 0
-
-x-bloks-version-id: 899adff463607d5f13a547f7417a9de4a8b4add115ddebc553c1bc5b8d48a28a
+x-client-doc-id: 124930351917786857261002920888
 
 x-fb-client-ip: True
 
-x-fb-connection-type: WIFI
+x-fb-friendly-name: IGUSDIDRegistrationMutation
 
-x-fb-friendly-name: IgApi: bloks/async_action/com.bloks.www.bloks.caa.login.async.send_login_request/
-
-x-fb-request-analytics-tags: {"network_tags":{"product":"567067343352427","surface":"undefined","request_category":"api","purpose":"fetch","retry_attempt":"0"}}
+x-fb-request-analytics-tags: {"network_tags":{"product":"567067343352427","request_category":"graphql","purpose":"none","retry_attempt":"0"}}
 
 x-fb-server-cluster: True
 
@@ -48,25 +32,11 @@ x-ig-app-id: 567067343352427
 
 x-ig-app-locale: en_US
 
-x-ig-attest-params: 
-
-x-ig-bandwidth-speed-kbps: -1.000
-
-x-ig-bandwidth-totalbytes-b: 0
-
-x-ig-bandwidth-totaltime-ms: 0
-
-x-ig-client-endpoint: com.bloks.www.caa.login.login_homepage:com.bloks.www.caa.login.login_homepage
-
 x-ig-capabilities: 3brTv10=
 
-x-ig-connection-type: WIFI
-
-x-ig-device-id: 4c3431e6-3663-480b-abfd-9bd6586a8dcc
+x-ig-device-id: 0ccec6fc-2663-4e9a-9854-df9ec61e8419
 
 x-ig-device-locale: en_US
-
-x-ig-family-device-id: c6a297ad-aa41-47a6-bb2e-a39ac6a76be4
 
 x-ig-is-foldable: false
 
@@ -74,15 +44,13 @@ x-ig-mapped-locale: en_US
 
 x-ig-timezone-offset: 21600
 
-x-ig-www-claim: 0
+x-ig-validate-null-in-legacy-dict: true
 
-x-mid: ac96XgABAAEvkGYmjNRwxyvq1nil
+x-meta-usdid: 24c03837-0fd1-44cf-a814-88658a343af1.1776241348.MEYCIQCeZfjiGc3bJmPRomKGB3UNtJpQInFzRYHuhy8SuhpUdwIhAJvt_1PIzQi-9RmOlaSoCnGx25J2ObdXnN8IVJhYirsa
 
-x-meta-usdid: 4fa59d0e-ac9d-452c-aea0-739884af576a.1775208562.MEUCIQCwUmqILboX1si_HYMPmvE6F63GnidNMKawYeLErZnhUQIgLmrDRFK-X7J5dVZtaeiBMsdJCVJZg87ST-uzoOCteac
+x-pigeon-rawclienttime: 1776237750.691
 
-x-pigeon-rawclienttime: 1775204977.618
-
-x-pigeon-session-id: UFS-aa278f51-b728-4428-9610-786215a8c3d4-0
+x-root-field-name: usdid_registration
 
 x-tigon-is-retry: False
 
@@ -92,10 +60,8 @@ x-zero-eh: IG0e09d776530888418ab70f3822fbb4e1
 
 accept-encoding: zstd
 
-content-length: 4090
-
-user-agent: Instagram 423.0.0.47.66 Android (30/11; 274dpi; 720x1640; Symphony; Z35; Z35; mt6765; en_US; 923309008)
-
-x-fb-conn-uuid-client: d33a1e61cca92515232d3dacc1731790
+x-fb-conn-uuid-client: 28e5643ec8dbebe8739a5660e0350c4f
 
 x-fb-http-engine: Tigon/MNS/TCP
+
+x-graphql-client-library: pando
